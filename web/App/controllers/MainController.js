504e79ae687d68bc233c6aca3024aaeafa472b97
zhiping <zhiping@uoko.com>
2016-04-21 14:20:54 +0800

    add eg

--- web/App/controllers/MainController.js
+++ web/App/controllers/MainController.js
@@ -6,11 +6,11 @@ function(consulApp){
      //处理单页 主页面相关 
     consulApp.controller('MainController', function ($scope,$rootScope,$http,$state,$stateParams) {
            
-            $scope.dataCenters=["DC01","DC02"]
-            $scope.currDataCenter=$scope.dataCenters[0];
+            $rootScope.dataCenters=["DC01","DC02"]
+            $scope.currDataCenter=$rootScope.dataCenters[0];
             $scope.ChangeDC=function name(dc) {
                 $scope.currDataCenter=dc;
-                $state.go("services");
+                $state.go("services",{dc:dc},{inherit:false});
             }
             
             
--- web/App/controllers/ServicesController.js
+++ web/App/controllers/ServicesController.js
@@ -6,8 +6,8 @@ define([
     consulApp.controller('ServicesController', function ($scope,$rootScope,$http,$state,$stateParams) {
             $rootScope.headTitle= "服务列表";
             
-            $scope.services=[]
-
+            $scope.services=[];
+            $scope.dc=$stateParams["dc"]? $stateParams["dc"]:$rootScope.dataCenters[0];
 
     })
 
