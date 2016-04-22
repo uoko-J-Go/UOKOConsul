9be70db4ff864ade738b4e5ec8236566533a155a
zhiping <zhiping@uoko.com>
2016-04-22 12:08:43 +0800

    update $httpProvider

--- web/App/app.js
+++ web/App/app.js
@@ -13,6 +13,48 @@ define([
     app.config(['$stateProvider', '$urlRouterProvider','$httpProvider', function($stateProvider, $urlRouterProvider,$httpProvider) {
     /*Http请求处理*/ 
     $httpProvider.interceptors.push('apiBaseAddressHandler');
+    $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
+    /**
+     * The workhorse; converts an object to x-www-form-urlencoded serialization.
+     * @param {Object} obj
+     * @return {String}
+     */ 
+    var param = function(obj) {
+        var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
+        
+        for(name in obj) {
+        value = obj[name];
+            
+        if(value instanceof Array) {
+            for(i=0; i<value.length; ++i) {
+            subValue = value[i];
+            fullSubName = name + '[' + i + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+            }
+        }
+        else if(value instanceof Object) {
+            for(subName in value) {
+            subValue = value[subName];
+            fullSubName = name + '[' + subName + ']';
+            innerObj = {};
+            innerObj[fullSubName] = subValue;
+            query += param(innerObj) + '&';
+            }
+        }
+        else if(value !== undefined && value !== null)
+            query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
+        }
+        
+        return query.length ? query.substr(0, query.length - 1) : query;
+    };
+    
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [function(data) {
+        return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+    }];
     /*Http请求处理 */  
     /*路由配置 */ 
     $urlRouterProvider.otherwise('/');
@@ -36,7 +78,7 @@ define([
         return {
             request: function (config) {
                 if (!/https?:\/\/(.*?)+/.test(config.url)&&config.url.indexOf("v1/") >= 0) {
-                    config.url = apiUrl + config.url;
+                    config.url = apiBaseAddress + config.url;
                 }
                 return config;
             }
